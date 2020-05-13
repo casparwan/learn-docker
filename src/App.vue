b375a4a3d5d99f3e4aef6ae371917aa68e5a2506
wanghaixin <111@qq.com>
2020-05-13 19:30:46 +0800

    nginx

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="i am docker. whx"/>
+    <HelloWorld msg="i am docker123"/>
   </div>
 </template>
 
