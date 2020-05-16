c91b005aea7d380c72ee6e98a83c521587664edb
caspar <casparwan@163.com>
2020-05-16 15:48:29 +0800

    whx

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="hello docker"/>
+    <HelloWorld msg="hello docker. whx"/>
   </div>
 </template>
 
