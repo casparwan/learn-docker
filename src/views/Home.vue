1a1b6fdac71ba74e010ed2a63c078aee1e966b7d
wanghaixin <111@qq.com>
2020-05-20 11:29:51 +0800

    haha
    1
    
    11

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,74 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span style="float: left;">账号管理</span>
+        <el-button style="float: right;" type="primary" @click="handleAdd()">新增</el-button>
+      </div>
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column prop="userName" label="用户名" width="180"></el-table-column>
+        <el-table-column prop="password" label="密码" width="180"></el-table-column>
+        <el-table-column fixed="right" label="操作" width="100">
+          <template slot-scope="scope">
+            <el-button type="primary" @click="handleEdit(scope.row)" size="small">编辑</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+    <modal :visible="visible"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Modal from '../components/Modal';
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    Modal
+  },
+  created() {
+    this.$http.get("api/v1/users").then(({ data }) => {
+      if (data.success) {
+        this.tableData = data.data;
+      }
+    });
+  },
+  data() {
+    return {
+      visible: false,
+      tableData: []
+    };
+  },
+  methods: {
+    handleAdd() {
+      this.visible = true;
+    }
+  }
+};
+</script>
+<style lang="less" dscoped>
+.container {
+  max-width: 800px;
+  margin: auto;
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both;
+  }
+
+  .box-card {
+    width: 480px;
   }
 }
-</script>
+</style>
