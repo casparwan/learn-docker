5a1da53c332babaa39981680d28d86575b0c58d1
wanghaixin <111@qq.com>
2020-05-21 10:49:20 +0800

    config

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -32,21 +32,12 @@ export default {
   methods: {
     save() {
       const body = this.form;
-    //   this.axios.post("/api/v1/users", body).then(({ data }) => {
-    //     if (data.success) {
-    //       this.visible = false;
-    //     }
-    //   });
-      fetch("/api/v1/users", {
-        method: "POST", // or 'PUT'
-        body: JSON.stringify(body), // data can be `string` or {object}!
-        headers: new Headers({
-          "Content-Type": "application/json"
-        })
-      })
-        .then(res => res.json())
-        .catch(error => console.error("Error:", error))
-        .then(response => console.log("Success:", response));
+      this.axios.post("/api/v1/users", body).then(({ data }) => {
+        if (data.success) {
+          this.visible = false;
+          
+        }
+      });
     }
   }
 };
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 
 module.exports = {
-    devServer: {
-      proxy: {
-        "/api": {
-          target: "http://localhost:1111",
-          changeOrigin: true
-        }
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:1111",
+        changeOrigin: true
       }
     }
-  };
+  }
+};
